cccb74e6ddaff431a55b4c83417ca3e7720dfcb2
Saba Gogrichiani <sabagogrichiani@gmail.com>
2024-11-04 21:04:44 +0400

    Initial Commit

---
+++ Taro-iOS/Taro_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Taro_iOSApp.swift
+//  Taro-iOS
+//
+//  Created by Saba Gogrichiani on 04.11.24.
+//
+
+import SwiftUI
+
+@main
+struct Taro_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
